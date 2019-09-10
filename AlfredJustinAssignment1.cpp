cb31f4891d238850dabb213a739779ce11e577b3
JTA0617 <55165508+JTA0617@users.noreply.github.com>
2019-09-10 18:04:11 -0500

    Add files via upload

---
+++ AlfredJustinAssignment1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int a, b, c;
+    
+    cout<<"Please enter two integers to add\n";
+    cin >> a >> b;
+    
+    c = a + b;
+    cout << "The sum of the numbers is: " << c << endl;
+
+    return 0;
+}
