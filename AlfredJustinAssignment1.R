e7d3cc1f1fea989f47debc74d7c8a1cb6ba77127
JTA0617 <55165508+JTA0617@users.noreply.github.com>
2019-10-10 18:03:08 -0500

    Create AlfredJustinAssignment1.R

---
+++ AlfredJustinAssignment1.R
@@ -0,0 +1,8 @@
+first <- readline(prompt = "Enter the first number: ")
+second <-  readline(prompt = "Enter the second number: ")
+
+    first <- as.integer(20)
+    second <- as.integer(21)
+result = first + second
+
+print(paste("The sum of ",first, "and ",second, " is ", result))
